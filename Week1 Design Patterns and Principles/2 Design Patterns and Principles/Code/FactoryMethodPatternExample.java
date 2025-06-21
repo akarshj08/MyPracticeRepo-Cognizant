5e71e2d51ce2082ff8979f5a4faf1d30940726f8
Akarsh Jain <160271579+akarshj08@users.noreply.github.com>
2025-06-21 21:18:00 +0530

    Adding Week 1 Hands-On
    Design Patterns and Principles

---
+++ Week1 Design Patterns and Principles/2 Design Patterns and Principles/Code/FactoryMethodPatternExample.java
@@ -0,0 +1,85 @@
+public class FactoryMethodPatternExample 
+{
+    public static void main(String[] args) 
+    {
+        Test object = new Test();
+        object.test();
+    }
+}
+
+interface Document 
+{
+    void open();
+}
+
+class WordDocument implements Document 
+{
+    public void open() 
+    {
+        System.out.println("Opening a Word document.");
+    }
+}
+
+class PdfDocument implements Document 
+{
+    public void open() 
+    {
+        System.out.println("Opening a PDF document.");
+    }
+}
+
+class ExcelDocument implements Document 
+{
+    public void open() 
+    {
+        System.out.println("Opening an Excel document.");
+    }
+}
+
+abstract class DocumentFactory 
+{
+    public abstract Document createDocument();
+}
+
+
+class WordDocumentFactory extends DocumentFactory 
+{
+    public Document createDocument() 
+    {
+        return new WordDocument();
+    }
+}
+
+class PdfDocumentFactory extends DocumentFactory 
+{
+    public Document createDocument() 
+    {
+        return new PdfDocument();
+    }
+}
+
+class ExcelDocumentFactory extends DocumentFactory 
+{
+    public Document createDocument() 
+    {
+        return new ExcelDocument();
+    }
+}
+
+class Test
+{
+    public void test()
+    {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
