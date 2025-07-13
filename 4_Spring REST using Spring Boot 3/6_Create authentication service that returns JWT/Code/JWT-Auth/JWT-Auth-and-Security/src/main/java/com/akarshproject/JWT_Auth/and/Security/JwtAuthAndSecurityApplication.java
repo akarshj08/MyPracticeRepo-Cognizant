19e0319e3e0c5b3de7a84378429b1a383836af7e
Akarsh Jain <160271579+akarshj08@users.noreply.github.com>
2025-07-13 22:54:07 +0530

    Add files via upload

---
+++ 4_Spring REST using Spring Boot 3/6_Create authentication service that returns JWT/Code/JWT-Auth/JWT-Auth-and-Security/src/main/java/com/akarshproject/JWT_Auth/and/Security/JwtAuthAndSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.akarshproject.JWT_Auth.and.Security;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthAndSecurityApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(JwtAuthAndSecurityApplication.class, args);
+	}
+}
